61e6e9d0ad849a9add6fa18b1266793d6e8eabf1
NiAshu2529 <bhardwajnidhi04@gmail.com>
2022-11-01 18:28:50 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Main from './Component/Main';
+import './Component/Style.css';
+
+
+
+
+function App() {
+  return (
+    <>
+     <Main />
+    
+    </>
+   
+
+    
+  );
+}
+
+export default App;
