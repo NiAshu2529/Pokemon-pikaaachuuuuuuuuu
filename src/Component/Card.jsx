fa512cf67d20ecea03540a28cd80b12f83152d20
NiAshu2529 <bhardwajnidhi04@gmail.com>
2022-11-10 20:40:41 +0530

    Added Name Label

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -11,6 +11,7 @@ const Card = ({ pokemon, loading,infoPokemon}) => {
                             <div className="card" key={item.id} onClick={()=>infoPokemon(item)}>
                                 <h2>{item.id}</h2>
                                 <img src={item.sprites.front_default} alt="" />
+                                <label>Name</label>
                                 <h2>{item.name}</h2>
                             </div>
                         </>
